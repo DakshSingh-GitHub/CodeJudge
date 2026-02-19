ed54dc7c9a404c6d9d16e5832337994fe4b8e3dc
Daksh Singh <daksh.singh.2407@gmail.com>
2026-02-19 16:43:58 +0530

    simplified the folder structure even more.

--- reserves/script.ps1
+++
@@ -1,6 +0,0 @@
-if (-not (Test-Path -Path "./judge-backend-fastapi" -PathType Container)) {
-    Write-Host "Directory ./judge-backend-fastapi not found. Expanding archive..."
-    Expand-Archive -Path "judge-backend-fastapi.zip" -DestinationPath "./judge-backend-fastapi"
-}
-cd judge-backend-fastapi/judge-backend
-python app.py
